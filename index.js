da29b14b4b367fabc5c37f54051bdfc36adeac8a
opaque-maniac <mbithimark8@gmail.com>
2023-11-04 10:49:15 +0300

    Wrote a few more views and updated the index file

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const expressSession = require('express-session');
 
 // declaring a new mongoose app
 const app = new express();
 
+// declaring a global variable to confirm if user is logged in
+global.loggedIn = null;
+
 // setting app with specific middleware
 app.set('view_engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({
+    secret: 'keyboard cat'
+}));
+app.use("*", (req, res, next) => {
+    loggedIn = req.session.userId;
+    next();
+});
 
 // not really connecting to a real mongodb database
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser : true});
