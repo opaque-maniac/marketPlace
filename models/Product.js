3481964e6b03dd5cfb3175ab3f5e1db4bfa604e3
opaque-maniac <mbithimark8@gmail.com>
2023-11-19 13:42:39 +0300

    Finished the product Schema

--- models/Product.js
+++ models/Product.js
@@ -1,36 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        requried : true
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
     },
-    description : {
-        type : String,
-        required : true
-    },
-    price : {
-        type : Number,
-        required : true
-    },
-    category : {type : String, required : true},
-    images : [
-        {
-            type : String,
-            required : true
-        }
-    ],
-    SKU : {
-        type : Number,
-        default : new Number,
-        required : true
-    },
-    owner : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'Customer',
-        required : true
-    }
+  ],
 });
 
-const Product = mongoose.model('Product', productSchema);
-module.exports = Product;
+module.exports = mongoose.model("Product", ProductSchema);
