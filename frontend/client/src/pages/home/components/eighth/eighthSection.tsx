aa524d381062aa0fa968b91fb9c145de5d46761d
opaque-maniac <mbithimark8@gmail.com>
2024-06-30 02:03:27 +0300

    Updated the home page a bit more

--- frontend/client/src/pages/home/components/eighth/eighthSection.tsx
+++ frontend/client/src/pages/home/components/eighth/eighthSection.tsx
@@ -1,10 +1,16 @@
+import PhoneIcon from "./icons/phoneIcon";
+import ShieldIcon from "./icons/shieldIcon";
+import TruckIcon from "./icons/truckIcon";
+
 const EighthSection = () => {
   return (
     <section className="md:h-145 flex md:flex-row flex-col justify-center items-center md:gap-8 gap-y-16">
       <div className="px-4 h-60 md:w-3/12 w-5/12">
         <div className="h-40 w-full flex justify-center items-center">
           <div className="w-20 h-20 bg-gray-300 rounded-full p-2">
-            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2"></div>
+            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2">
+              <TruckIcon />
+            </div>
           </div>
         </div>
         <div className="text-center">
@@ -15,7 +21,9 @@ const EighthSection = () => {
       <div className="px-4 h-60 md:w-3/12 w-5/12">
         <div className="h-40 w-full flex justify-center items-center">
           <div className="w-20 h-20 bg-gray-300 rounded-full p-2">
-            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2"></div>
+            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2">
+              <PhoneIcon />
+            </div>
           </div>
         </div>
         <div className="text-center">
@@ -26,7 +34,9 @@ const EighthSection = () => {
       <div className="px-4 h-60 md:w-3/12 w-5/12">
         <div className="h-40 w-full flex justify-center items-center">
           <div className="w-20 h-20 bg-gray-300 rounded-full p-2">
-            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2"></div>
+            <div className="w-full h-full bg-black rounded-full flex justify-center items-center p-2">
+              <ShieldIcon />
+            </div>
           </div>
         </div>
         <div className="text-center">
