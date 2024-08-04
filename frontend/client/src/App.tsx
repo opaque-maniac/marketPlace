564452a3b75b14a6ff401a4aac2724a06e5d9ceb
opaque-maniac <mbithimark8@gmail.com>
2024-08-04 20:21:37 +0300

    Touched something, not a major change

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { lazy, Suspense, useState } from "react";
 import Header from "./components/header";
