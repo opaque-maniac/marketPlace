de1062772ed6f5f32bfd815fb836a75979efa70a
opaque-maniac <mbithimark8@gmail.com>
2024-08-04 20:09:50 +0300

    Added a rate limiter for the api

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ import customerRouter from "./customer/router";
 import sellerRouter from "./seller/router";
 import staffRouter from "./staff/router";
 import path from "path";
+import slowDown from "express-slow-down";
 
 const app = express();
 
@@ -14,9 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+const limiter = slowDown({
+  windowMs: 15 * 60 * 1000, // 5 minutes
+  delayAfter: 15,
+  delayMs: (hits) => hits * 200,
+  maxDelayMs: 5000, // max global delay of 5 seconds
+});
+
+// Rate Limiter
+app.use(limiter);
+
 // Options for cors
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "http://localhost:3001"],
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
